2a3db994372fea62becb928c3281ee0c68c33dfb
Fahreza Pasha Haikal <abufahreza@gmail.com>
2024-02-01 14:37:37 +0700

    update mobile navbar

--- src/layouts/MobileLayout.vue
+++ src/layouts/MobileLayout.vue
@@ -14,7 +14,7 @@ const toggleDark = useToggle(isDark);
     <nav class="fixed inset-x-0 bottom-0 border-t-2 bg-slate-50 dark:bg-dark drop-shadow-md dark:border-zinc-700">
       <div class="relative flex h-[70px] px-4 overflow-y-hidden">
         <div class="flex flex-1 flex-col relative min-w-0 items-center">
-          <router-link to="home" active-class="active !font-semibold"
+          <router-link :to="{name: 'home'}" active-class="active !font-semibold"
             class="relative top-1/2 h-20 w-20 shrink-0 -translate-y-1/2 rounded-full">
             <div
               class="absolute left-1/2 top-1/2 flex -translate-x-1/2 -translate-y-1/2 flex-col items-center justify-center">
@@ -41,17 +41,17 @@ const toggleDark = useToggle(isDark);
         </div>
         
         <div class="flex flex-1 flex-col relative min-w-0 items-center">
-          <button @click="toggleDark()" class="relative top-1/2 h-20 w-20 shrink-0 -translate-y-1/2 rounded-full">
+          <router-link to="about" active-class="active !font-semibold"
+            class="relative top-1/2 h-20 w-20 shrink-0 -translate-y-1/2 rounded-full">
             <div
               class="absolute left-1/2 top-1/2 flex -translate-x-1/2 -translate-y-1/2 flex-col items-center justify-center">
               <div class="flex h-8 w-8 shrink-0 items-center justify-center rounded-full p-1 bg-hover">
-                <Icon icon="mingcute:sun-line" width="25" class="dark:!hidden" />
-                <Icon icon="mingcute:moon-line" width="25" class="!hidden dark:!inline-block dark:text-white" />
+                <Icon icon="icon-park-solid:people"  
+                  class="text-dark dark:text-white h-full w-full" />
               </div>
-              <div class="shrink-0 text-xs dark:!hidden">Light</div>
-              <div class="shrink-0 text-xs text-white !hidden dark:!inline-block">Dark</div>
+              <div class="shrink-0 text-xs dark:text-white">Profil</div>
             </div>
-          </button>
+          </router-link>
         </div>
       </div>
     </nav>
