09e5152f11cf283d359154418b5cfcdd3c6308ed
Fahreza Pasha Haikal <abufahreza@gmail.com>
2024-03-19 01:47:57 +0700

    update for performance web

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,18 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
 import Sitemap from 'vite-plugin-sitemap'
+import Pages from 'vite-plugin-pages'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    Sitemap()
+    Sitemap({
+      hostname: 'http://chemicfest.com',
+      readable: true,
+    }),
+    Pages(),
+    VitePWA()
   ],
 });
