e17040d6fc07be1f1912ea48dfe564ce27a8d15c
Fahreza Pasha Haikal <abufahreza@gmail.com>
2024-03-19 01:13:52 +0700

    update Sitemap

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,12 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
-import Pages from "vite-plugin-pages";
 import Sitemap from 'vite-plugin-sitemap'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    Sitemap({
-      hostname: 'http://www.test.com',
-      readable: true,
-      dynamicRoutes,
-      allowRobots: true,
-    }),
-    Pages({
-      onRoutesGenerated: (routes) => generateSitemap({ routes }),
-    }),
+    Sitemap()
   ],
 });
