ca4e8a34480cf0c3a49f61b6ce473c724c66622d
Fahreza Pasha Haikal <abufahreza@gmail.com>
2024-03-19 19:25:59 +0700

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -8,6 +8,7 @@ import axios from 'axios';
 export default {
   data() {
     return {
+      response: null
     };
   },
   async mounted() {
@@ -16,7 +17,7 @@ export default {
         setInterval(() => { // Simpan ID interval
           this.sessionCheck()
 
-        }, 1000)      
+        }, 5000)      
       }
     } catch (error) {
       console.error(error);
@@ -24,18 +25,34 @@ export default {
   },
   methods: {
     async sessionCheck() {
-      const sessionId = localStorage.getItem('sessionId')
-      const user  = localStorage.getItem('username')
+      try {
+        const sessionId = localStorage.getItem('sessionId')
+        const user  = localStorage.getItem('username')
+        const isLoggedIn = !!localStorage.getItem('sessionId');
+  
+        if (sessionId && user) {
+          this.response = await axios.post('https://chemicfest.site/api/session/main/check', {
+            sessionid: sessionId,
+            users: user
+          })
+          console.log(this.response.data)
+        }
 
-      if (sessionId && user) {
-        const response = await axios.post('https://chemicfest.site/api/session/main/check', {
-          sessionid: sessionId,
-          users: user
-        })
+      } catch (error) {
+        if(error.response.status === 400) {
+          document.cookie = 'sessionId=; expires=Thu, 01 Jan 1970 00:00:00 UTC;';
 
-        this.sessionCheckResponse = response.data;
-        console.log(this.sessionCheckResponse)
-      }
+          localStorage.removeItem('sessionId');
+          localStorage.removeItem('username');
+          localStorage.removeItem('role');
+          localStorage.removeItem('phone');
+          localStorage.removeItem('email');
+          localStorage.removeItem('uuid');
+          this.$router.push('/login')
+          console.log('Session Expired')
+        }
+        console.error(error.response.status);
+      }      
     }
   }
 };
